d21f015d9e19b7cc309db27317a8cd09c4233921
Dan Moore <dan@makeitdoathing.com>
2015-01-04 13:12:13 -0500

    updates for unit tests

--- example/src/testApp.cpp
+++ example/src/testApp.cpp
@@ -1,61 +1,12 @@
 #include "testApp.h"
 
-#include "ofxGoogleTest.h"
-
-TEST(MyFirstTest, my_first_test)
-{
-	ASSERT_EQ(0, 0);
-	ASSERT_NE(0, 1);
-	ASSERT_TRUE(true);
-	ASSERT_FALSE(false);
-}
-
-TEST(MyFailedTest, my_failed_test)
-{
-	ASSERT_TRUE(false);
-}
-
-struct Vec3Test : testing::Test
-{
-protected:
-	
-	ofVec3f v0, v1;
-	
-	Vec3Test()
-	{
-		cout << "!!! 0" << endl;
-		v0.set(0, 0, 0);
-		v1.set(10, 10, 10);
-	}
-	
-	void SetUp()
-	{
-		cout << "!!! 1" << endl;
-	}
-	
-	void TearDown()
-	{
-		cout << "!!! 2" << endl;
-	}
-	
-	~Vec3Test()
-	{
-		cout << "!!! 3" << endl;
-	}
-};
-
-TEST_F(Vec3Test, eq_test)
-{
-	cout << "!!! test start" << endl;
-	ASSERT_EQ(v0, ofVec3f(0, 0, 0));
-	ASSERT_EQ(v1, ofVec3f(10, 10, 10));
-	cout << "!!! test end" << endl;
-}
+#include "ofVec3fTest.h"
+#include "ofMeshTest.h"
 
 //--------------------------------------------------------------
 void testApp::setup()
 {
-	ofExit(ofxGoogleTest::run());
+	ofxGoogleTest::run();
 }
 
 //--------------------------------------------------------------
